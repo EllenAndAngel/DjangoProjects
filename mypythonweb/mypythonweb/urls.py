9aa345dbd59cf121d8b3715e6b6fa5de28b203a5
EllenZhang <developer.ellenzhang@gmail.com>
2017-10-11 17:42:28 +1300

    Test something

--- mypythonweb/mypythonweb/urls.py
+++ mypythonweb/mypythonweb/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home)
+    url(r'^$', views.home),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

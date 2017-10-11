20f829628ae63985a536b4e945abc72d39508ef0
EllenZhang <developer.ellenzhang@gmail.com>
2017-10-11 17:31:43 +1300

    update something

--- mypythonweb/posts/models.py
+++ mypythonweb/posts/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
 class Post(models.Model):
-	"""docstring for Post"""
-	def __init__(self, arg):
-		super(Post, self).__init__()
-		self.arg = arg
-	
-	title = models.CharField(max_length = 250)
-	pub_date = models.DateTimeField()
-	image = models.ImageField(upload_to = 'media/')
-	body = models.TextField()
+    """docstring for Post"""
+
+    title = models.CharField(max_length=250)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='media/')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def summary(self):
+        return self.body[:100]
+
